341f72526364516df866d74e77aab18276bbee9f
alexx sub <alexx@ALEXX-YOGA>
2023-03-26 09:44:33 +0300

    addphone

--- vue-phones/src/App.vue
+++ vue-phones/src/App.vue
@@ -1,28 +1,42 @@
 <script setup>
 import { ref } from "vue";
 //Объявляем переменную реактивной
-const newPhone = ref("");
+const inputnumber = ref(null);
+//объявляем переменную для хранения новой записи
+const newPhone = ref({
+  number: "",
+  name: "",
+});
 //массив с данными, обычный массив и его тоже объявляем реактивным
 const phones = ref([]);
 function addPhone() {
   //видимость переменных получаем без this
   phones.value.push(newPhone.value); //нам не надо заботится о выводе новых данных
   //как только элемент будет добавлен в массив, он появится в списке
-  newPhone.value = ""; // затираем переменную ввода
+  newPhone.value = { number: "", name: "" }; // затираем переменную ввода
+  inputnumber.value.focus(); //переносим фокус в поле номер
 }
 </script>
 
 <template>
   <div>
-    <!-- Описываем поле вводы и вешаем обработчик на нажатие клавиши Enter -->
+    <!-- Описываем поле ввода номера -->
     <input
+      ref="inputnumber"
+      v-model="newPhone.number"
       placeholder="Введите номер"
-      v-model="newPhone"
+
+    />
+     <!--Добавляем еще одно поле ввода данных - имя и вешаем обработчик на нажатие клавиши Enter-->
+     <input
+      placeholder="Введите имя"
+      v-model="newPhone.name"
       @keyup.enter="addPhone"
     />
     <ul>
       <!-- Уже знакомый вывод списком-->
-      <li v-for="phone in phones" :key="phone">{{ phone }}</li>
+      <li v-for="phone in phones" :key="phone">
+        {{ phone.number }} {{ phone.name }}</li>
     </ul>
   </div>
 </template>
