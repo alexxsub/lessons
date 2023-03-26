7376be0fbd7f65fe148ba12b383d24178dc56058
alexx sub <alexx@ALEXX-YOGA>
2023-03-26 08:38:04 +0300

    fix

--- apollo-graphql/index.js
+++ apollo-graphql/index.js
@@ -1,13 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-
+//пример данных, массив с телефонами
 var _phones = [
   { number: "5555", name: "John" },
   { number: "6666", name: "Bill" },
   { number: "7777", name: "Smith" },
   { number: "1234", name: "Sara" }
 ];
-// Construct a schema, using GraphQL schema language
+// Описываем схему, используя sgl язык
 const typeDefs = `
   type Phone {
     number: String
@@ -18,21 +18,24 @@ const typeDefs = `
   }
 `;
 
-// Provide resolver functions for your schema fields
+// Описываем резолвер для метода просмотра
 const resolvers = {
   Query: {
     Phones: () => _phones
   }
 };
 
+//регистрируем схему и резолверы
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-
+//создаем экземляр сервера
+const HOST = process.argv[2];
+const PORT = process.argv[3];
 const { url } = await startStandaloneServer(server, {
-  listen: { host:"localhost",port: 9000 },
+  listen: { host:HOST,port: PORT },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
