4e47b88e0830dfb27352b8b041a708da15d2edab
aLexx Sub <alexei.s@mail.ru>
2023-05-06 23:38:55 +0300

    vue vite

--- vue-phones/src/App.vue
+++ vue-phones/src/App.vue
@@ -1,64 +1,17 @@
 <script setup>
-import { ref,reactive, onMounted } from "vue";
+import { ref,reactive} from "vue";
 
 
 //Объявляем переменную реактивной
 const inputnumber = ref(null);
 //объявляем переменную для хранения новой записи
-var newPhone = reactive({
+let inputPhone = reactive({
   number: "",
   name: "",
+  id:""
 });
 //массив с данными, обычный массив и его тоже объявляем реактивным
-const phones = reactive([]);
-//флаг режима, режим true - правка данных, false -ввод новых
-const editmode = ref(false);
-//индекс редактируемого элемента 
-const index = ref(-1); 
-
-function addPhone() {
-  //видимость переменных получаем без this
-  phones.push(newPhone); //нам не надо заботится о выводе новых данных
-  //как только элемент будет добавлен в массив, он появится в списке
-  newPhone = { number: "", name: "" }; // затираем переменную ввода
-  inputnumber.value.focus(); //переносим фокус в поле номер
-}
-
-function savePhone() {
-  //сохраняем данные, только если индекс имеется, т.е. >-1
-  if (index.value > -1) {
-    Object.assign(phones[index.value], newPhone);
-    resetPhone(); //очищаем все поля
-  }
-}
-
-function resetPhone() {
-  //тут обнуляем переменные и приводим все в исходное состояние
-  newPhone = { number: "", name: "" }; // затираем переменную ввода
-  inputnumber.value.focus();
-  editmode.value = false; //выключаем режим редактирования
-  index.value = -1;
-}
-
-function deletePhone(item) {
-  //ищем выбранный элемент массива и удаляем
-  const index = phones.value.indexOf(item);
-  phones.value.splice(index, 1);
-}
-
-function setPhone(item) {
-  // данная функция при режиме редактирования устанавливает в полях данные для редактирования
-  //вычисляем индекс и сохраняем в переменной
-  index.value = phones.indexOf(item);
-  //для вывода данных в полях, выводим их в связных переменных
-  newPhone = Object.assign({}, item);
-  //включаем режим редактирования, появляются кнопки
-  editmode.value = true;
-}
-//первичная инициализация
-onMounted(() => {
-  // инициализация демо данными
-  phones.value = [
+const phones = reactive( [
     {
       number: "8903888777666",
       name: "Петя",
@@ -71,10 +24,66 @@ onMounted(() => {
       number: "8909222333888",
       name: "Маша",
     },
-  ];
+  ]);
+//флаг режима, режим true - правка данных, false -ввод новых
+const editmode = ref(false);
+
+
+
   //доббавляем в демо данные id
-phones.value.map(i=>i.id=Date.now().toString(36) + Math.random().toString(36).slice(2));
-});
+phones.map(i=>i.id=getID());
+
+function getID(){
+  return Date.now().toString(36) + Math.random().toString(36).slice(2)
+}
+
+function addPhone() {
+  
+  let newPhone=inputPhone
+  newPhone.id=getID()
+  //видимость переменных получаем без this
+  phones.push(newPhone); //нам не надо заботится о выводе новых данных
+  //как только элемент будет добавлен в массив, он появится в списке
+  resetPhone()
+}
+
+function savePhone() {
+  //сохраняем данные, только если id не пустой
+  if (inputPhone.id != '') {
+    const index=phones.findIndex(x => x.id === inputPhone.id)
+    phones.splice(index, 1, inputPhone);
+    resetPhone(); //очищаем все поля
+  }
+}
+
+function resetPhone() {
+  //тут обнуляем переменные и приводим все в исходное состояние
+  inputPhone={
+  number: "",
+  name: "",
+  id:""
+}
+  inputnumber.value.focus();
+  editmode.value = false; //выключаем режим редактирования
+
+}
+
+function deletePhone(item) {
+  //ищем выбранный элемент массива и удаляем
+  const index = phones.indexOf(item);
+  
+  phones.splice(index, 1);
+}
+
+function setPhone(item) {
+  // данная функция при режиме редактирования устанавливает в полях данные для редактирования
+ 
+  //для вывода данных в полях, выводим их в связных переменных
+  inputPhone = Object.assign({}, item);
+  //включаем режим редактирования, появляются кнопки
+  editmode.value = true;
+}
+
 </script>
 
 <template>
@@ -82,25 +91,24 @@ phones.value.map(i=>i.id=Date.now().toString(36) + Math.random().toString(36).sl
     <!-- Описываем поле ввода номера -->
     <input
       ref="inputnumber"
-      v-model="newPhone.number"
+      v-model="inputPhone.number"
       placeholder="Введите номер"
 
     />
      <!--Добавляем еще одно поле ввода данных - имя и вешаем обработчик на нажатие клавиши Enter-->
      <input
       placeholder="Введите имя"
-      v-model="newPhone.name"
+      v-model="inputPhone.name"
       @keyup.enter="addPhone"
     />
     <button v-show="editmode" @click="savePhone">Сохранить</button>
     <button v-show="editmode" @click="resetPhone">Отмена</button>
-    <ul>
-      <!-- Уже знакомый вывод списком-->
-      <li v-for="phone in phones" :key="phone">
-        <a href="#" @click="setPhone(phone)">{{ phone.id }} {{ phone.number }}</a>
-        {{ phone.name }}
-        <button @click="deletePhone(phone)">x</button>
-      </li>
-    </ul>
+    <table width="350px">
+      <tr v-for="phone in phones" :key="phone.id">
+        <td><a href="#" @click="setPhone(phone)">{{ phone.number }}</a></td>
+        <td>{{ phone.name }}</td>
+        <td><button @click="deletePhone(phone)">x</button></td>
+      </tr>
+    </table>
   </div>
 </template>
